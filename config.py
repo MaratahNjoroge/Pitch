36d76289fc6bca79167f2e8318c02b99421eef55
Maratah <maratah7@gmail.com>
2020-07-17 16:30:54 +0300

    add inputs to the file.

--- config.py
+++ config.py
@@ -4,9 +4,20 @@ class Config:
     '''
     General configuration parent class
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://username:password@localhost/pitch'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
 
+    SECRET_KEY = "soml"
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+     # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
     '''
     Production configurations child class
     '''
@@ -17,3 +28,9 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+    
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
