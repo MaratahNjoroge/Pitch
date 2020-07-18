46065fcc412641df20d6fc9a350f9a0f6c57e986
Maratah <maratah7@gmail.com>
2020-07-18 16:38:22 +0300

    deploy to heroku.

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     '''
     Production configurations child class
     '''
